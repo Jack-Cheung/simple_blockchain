cea8a38fe390c009f453680c8be898366dbde29f
Jack-Cheung <374511110@qq.com>
2019-04-09 09:24:48 +0800

    before my coding

---
+++ plugins/chain_plugin/include/config.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace eosio{namespace chain_apis{
+
+    //constexpr char MYCONTRACT_ABI_PATH[] = "/home/qw/github/simple_blockchain/build/plugins/chain_plugin/../../contracts/mycontract/mycontract.abi";
+    //constexpr char COMPRESSED_MYCONTRACT_ABI[] = "{\"account_name\":\"mycontract\",\"abi\":}";
+    //constexpr string MYCONTRACT_PRIVATE_KEY("5K5iT9EdUCQbS7uCqa5LVFJSpVE3UDXs1yYXTy1j5FuzEaE4PWV");
+    #define MYCONTRACT_PRIVATE_KEY "5K5iT9EdUCQbS7uCqa5LVFJSpVE3UDXs1yYXTy1j5FuzEaE4PWV"
+    #define EOS_DEVELOP_KEY "EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV"
+}};
